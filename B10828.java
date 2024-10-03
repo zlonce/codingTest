730436ad05711478aa20be978d63edcf5f40c09c
zlonce <zlon@outlook.kr>
2024-10-03 21:13:21 +0900

    b1828

--- B10828.java
+++ B10828.java
@@ -1,7 +1,8 @@
 //백준 10828번
 import java.util.*;
+import java.io.*;
 
-private class Node{
+class Node{
   int data;
   Node prev;
 
@@ -11,21 +12,25 @@ private class Node{
    }
 }
 
-public class Stack{
+private class Stack{
   private Node top;
 
   public void push(int data){
-     new newNode = newNode(data);
+     Node newNode = new Node(data);
      newNode.prev = top;
      top = newNode;
   }
 
-  public int pop(){
-    System.out.println(top.data);
+  public void pop(){
+    if(empty()){
+      System.out.println(-1);
+    }
+    else{System.out.println(top.data);
     top.prev = top;
+    }
   }
 
-  public int boolean empty(){
+  public void empty(){
     if(top.prev == null){
       System.out.println(1);
     }else{
@@ -35,40 +40,39 @@ public class Stack{
  
   public void size(){
     int size = 0;
-    for(top; top.prev != null; top ->top.prev){
-      size += 1;
+    for(Node curr = top; curr.prev != null; curr = curr.prev){
+      size ++;
     }
     System.out.println(size);
   }
   
   public void top(){
+    if(empty()){
+      System.out.println(-1);
+      return -1;
+    }
     System.out.println(top.data);
   }
 }
 
 public class B10828{
   public static void main(String args[]) throws IOException{
-    bufferedReader br = new BufferedReader(new InputStreamReader(System.in){
-      int N = Integer.valueOf(br.readLine());
-      stack myStack = new Stack();
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.valueOf(br.readLine());
+    Node myStack = new Stack();
 
-      for(int i = 0; i < N; i++){
-        String[] input = br.readLine().split(" ");
-        String oder = input[0];
+    for(int i = 0; i < N; i++){
+      String[] input = br.readLine().split(" ");
+      String oder = input[0];
 
-        if(oder.equals("push"){
-          int num = input[1];
-          myStack.push(num);
-        }
-        else if(oder.equals("pop") myStack.pop();
-        else if(oder.equals("size") myStack.size();
-        else if(oder.equals("empty") myStack.empty();
-        else if)oder.equals("top") myStack.top();
+      if(oder.equals("push")){
+        int num = integer.parseInt(input[1]);
+        myStack.push(num);
       }
-
-      static boolean empty(){
-        return top == null;
-      }
+      else if(oder.equals("pop")) myStack.pop();
+      else if(oder.equals("size")) myStack.size();
+      else if(oder.equals("empty")) myStack.empty();
+      else if(oder.equals("top")) myStack.top();
     }
   }
 }
